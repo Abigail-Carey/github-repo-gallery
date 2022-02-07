654e9830dcca5d042a178607cbbbce4ce7989bc0
abigail_c <abigailcarey9@gmail.com>
2022-02-07 16:56:49 -0500

    Added user data

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// overview div is where your profile info will appear
+const overview = document.querySelector(".overview")
+const username = "Abigail-Carey";
+
+const getData = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    //console.log(data);
+    displayData(data);
+};
+getData();
+
+const displayData = function (data) {
+    const div = document.createElement("div");
+    div.classList.add(".user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
+    overview.append(div)
+};
